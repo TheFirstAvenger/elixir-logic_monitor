4ead4286ab198c23c2a1ad5188a0caba57d48b58
Mike Binns <mike@mikebinns.net>
2017-03-28 16:28:06 -0400

    v0.0.9

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LogicMonitor.Mixfile do
 
   def project do
     [app: :logic_monitor,
-     version: "0.0.8",
+     version: "0.0.9",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
